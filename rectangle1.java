5455fce4bb54490f7837164e22aaec962bbc63e2
aasimshaikh95 <49490064+aasimshaikh95@users.noreply.github.com>
2019-09-23 09:42:48 +0530

    Add files via upload

---
+++ rectangle1.java
@@ -0,0 +1,38 @@
+class rectangle1
+{	
+	double label,b,area;
+	rectangle()
+	{
+	l=1;
+	b=1;
+	}
+	rectangle(double len,double bre)
+	{
+		l=len;
+		b=bre;
+	}	
+	void area()
+	{
+		areaa=l*b;
+	}
+	void perimeter()
+	{
+		peri=
+	}
+
+	void display()
+	{
+		System.out.println("Area of rectangle: "+ area);
+	}
+
+	/*void get()
+	{
+		System.out.println("Length= "+ l + "Breadth= "+b);
+	}
+	void set(double ln,double br)
+	{
+		l=ln;
+		b=br;
+	}*/
+
+}
